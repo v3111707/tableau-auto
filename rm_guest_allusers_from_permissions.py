a9f2689bcf557c1113cf8dc16af69d49ea023c8b
Sergey Korneev <s_korneev@wargaming.net>
2023-04-26 16:13:30 +0300

    rm_guest_allusers_from_permissions.py

--- rm_guest_allusers_from_permissions.py
+++ rm_guest_allusers_from_permissions.py
@@ -14,26 +14,25 @@ Options:
 """
 
 import logging
-import sys
 import json
 import os
 import tableauserverclient as TSC
-import xml.etree.ElementTree as etree
 from configparser import ConfigParser
 from docopt import docopt
 from logging.handlers import TimedRotatingFileHandler
-
 
 SCRIPT_HOME = os.path.dirname(os.path.realpath(__file__))
 cred_file = os.path.join(SCRIPT_HOME, 'ad2tabsync.conf')
 conf_file = os.path.join(SCRIPT_HOME, 'rm_guest_allusers_from_permissions.json')
 logs_file = os.path.join(SCRIPT_HOME, 'rm_guest_allusers_from_permissions.log')
 
+
 def _read_conf_file(filename):
     lgr = logging.getLogger('main.read_conf_file')
     lgr.debug(f'Try to read {filename}')
     with open(filename) as f:
         return json.load(f)
+
 
 class TableauPermissionCleaner():
     def __init__(self, url, username, password, noop: bool = True):
@@ -46,25 +45,29 @@ class TableauPermissionCleaner():
         self.pwd = password
         self.tsc_server = TSC.Server(server_address=url)
 
-
     def _clear_site_from_group(self, ignore_tag, group_id):
         self.lgr.debug(f'Run _clear_site_from_group with ignore_tag:{ignore_tag}, group_id:{group_id}')
 
-        workbooks = [w for w in list(TSC.Pager(self.tsc_server.workbooks)) if ignore_tag not in w.tags]
-        for w in workbooks:
-            self.tsc_server.workbooks.populate_permissions(w)
-            for p in w.permissions:
+        # workbooks = [w for w in list(TSC.Pager(self.tsc_server.workbooks)) if ignore_tag not in w.tags]
+        for wb in TSC.Pager(self.tsc_server.workbooks):
+            self.lgr.debug(f'Proccesing "{wb.name}"')
+            if ignore_tag in wb.tags:
+                continue
+            self.tsc_server.workbooks.populate_permissions(wb)
+            for p in wb.permissions:
                 if p.grantee.id == group_id and p.grantee.tag_name == 'group':
-                    self.lgr.info(f'Remove {str(p.capabilities)} permissions from wb: "{w.name}" , project: "{w.project_name})"')
+                    self.lgr.info(
+                        f'Remove {str(p.capabilities)} permissions from wb: "{wb.name}" , project: "{wb.project_name})"')
                     if not self.noop:
-                        self.tsc_server.workbooks.delete_permission(w, p)
+                        self.tsc_server.workbooks.delete_permission(wb, p)
 
         datasources = [d for d in list(TSC.Pager(self.tsc_server.datasources)) if ignore_tag not in d.tags]
         for d in datasources:
             self.tsc_server.datasources.populate_permissions(d)
             for p in d.permissions:
                 if p.grantee.id == group_id and p.grantee.tag_name == 'group':
-                    self.lgr.info(f'Remove {str(p.capabilities)} permissions from datasource: "{d.name}" , project: "{w.project_name})"')
+                    self.lgr.info(
+                        f'Remove {str(p.capabilities)} permissions from datasource: "{d.name}" , project: "{w.project_name})"')
                     if not self.noop:
                         self.tsc_server.datasources.delete_permission(w, p)
 
@@ -76,7 +79,8 @@ class TableauPermissionCleaner():
             self.tsc_server.workbooks.populate_permissions(w)
             for p in w.permissions:
                 if p.grantee.id == user_id and p.grantee.tag_name == 'user':
-                    self.lgr.info(f'Remove {str(p.capabilities)} permissions from wb: "{w.name}" , project: "{w.project_name})"')
+                    self.lgr.info(
+                        f'Remove {str(p.capabilities)} permissions from wb: "{w.name}" , project: "{w.project_name})"')
                     if not self.noop:
                         self.tsc_server.workbooks.delete_permission(w, p)
 
@@ -85,18 +89,19 @@ class TableauPermissionCleaner():
             self.tsc_server.datasources.populate_permissions(d)
             for p in d.permissions:
                 if p.grantee.id == user_id and p.grantee.tag_name == 'user':
-                    self.lgr.info(f'Remove {str(p.capabilities)} permissions from datasource: "{d.name}" , project: "{d.project_name})"')
+                    self.lgr.info(
+                        f'Remove {str(p.capabilities)} permissions from datasource: "{d.name}" , project: "{d.project_name})"')
                     if not self.noop:
                         self.tsc_server.datasources.delete_permission(w, p)
 
     def _get_group_id_by_name(self, name):
-        groups = [g.id for g in list(TSC.Pager(self.tsc_server.groups))if g.name == name]
+        groups = [g.id for g in list(TSC.Pager(self.tsc_server.groups)) if g.name == name]
         if groups:
             return groups.pop()
         return None
 
     def _get_user_id_by_name(self, name):
-        users = [i.id for i in list(TSC.Pager(self.tsc_server.users))if i.name == name]
+        users = [i.id for i in list(TSC.Pager(self.tsc_server.users)) if i.name == name]
         if users:
             return users.pop()
         return None
@@ -118,6 +123,7 @@ class TableauPermissionCleaner():
             else:
                 self.lgr.warning(f'{clean_name} not found. Stop processing site: {site_id}')
 
+
 def main():
     lgr = logging.getLogger('main')
     argz = docopt(__doc__)
@@ -137,8 +143,9 @@ def main():
     config_parcer = ConfigParser()
     config_parcer.read_file(open(cred_file))
     cred = config_parcer['Tableau']
-    tbl_cleaner = TableauPermissionCleaner(url=cred['server'], username=cred['username'], password=cred['password'], noop=argz.get('--noop'))
-    for k,v in conf.items():
+    tbl_cleaner = TableauPermissionCleaner(url=cred['server'], username=cred['username'], password=cred['password'],
+                                           noop=argz.get('--noop'))
+    for k, v in conf.items():
         for site in v.get('sites'):
             tbl_cleaner.start_clean_site(site_id=site, ignore_tag=v.get('tag'), clean_name=k, group=v.get('group'))
 
