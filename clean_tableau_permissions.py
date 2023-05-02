e9c6ebb7135c9192cca2904e6502f2946f0c6b82
Sergey Korneev <s_korneev@wargaming.net>
2023-05-02 12:55:33 +0300

    clean_tableau_permissions.py

--- clean_tableau_permissions.py
+++ clean_tableau_permissions.py
@@ -5,11 +5,14 @@ import yaml
 import os
 import sys
 import typer
+import re
 import tableauserverclient as TSC
 from configparser import ConfigParser
 from typing import Optional
 from logging.handlers import TimedRotatingFileHandler
+from pyzabbix import ZabbixMetric, ZabbixSender
 
+SCRIPT_NAME = os.path.basename(__file__)
 SCRIPT_HOME = os.path.dirname(os.path.realpath(__file__))
 CRED_FILE = os.path.join(SCRIPT_HOME, 'ad2tabsync.conf')
 CONF_FILE = os.path.join(SCRIPT_HOME, 'clean_tableau_permissions.yaml')
@@ -41,6 +44,23 @@ def get_creds(file: str):
     config_parcer = ConfigParser()
     config_parcer.read_file(open(file))
     return config_parcer
+
+
+class Zabbix_sender(object):
+    def __init__(self, item_key: str, config_file:str = '/etc/zabbix/zabbix_agentd.conf'):
+        self.logger = logging.getLogger('main.Zabbix_sender')
+        self.item_key = item_key
+        zabbix_config = open(config_file).read()
+        self.server = re.search(r'ServerActive=(.+)', zabbix_config).group(1)
+        self.logger.debug(f"self.server: {self.server}")
+        self.hostname = re.search(r'Hostname=(.+)', zabbix_config).group(1)
+        self.logger.debug(f"self.hostname: {self.hostname}")
+
+    def send(self, value):
+        packet = [ZabbixMetric(self.hostname, self.item_key, value)]
+        self.logger.info(f"Send {packet} to {self.server}")
+        return ZabbixSender(zabbix_server=self.server).send(packet)
+
 
 
 class TableauPermissionCleaner:
@@ -201,7 +221,8 @@ app = typer.Typer(add_completion=False)
 
 @app.command(context_settings=dict(help_option_names=["-h", "--help"]))
 def main(debug: Optional[bool] = typer.Option(False, '-d', '--debug', show_default=True),
-         noop: Optional[bool] = typer.Option(False, '--noop', show_default=True)):
+         noop: Optional[bool] = typer.Option(False, '--noop', show_default=True),
+         zab_test: Optional[bool] = typer.Option(False, '--zt', show_default=True)):
     init_logger(debug=debug, file=LOG_FILE)
     log = logging.getLogger('main')
     log.debug('Debug mode')
@@ -211,6 +232,12 @@ def main(debug: Optional[bool] = typer.Option(False, '-d', '--debug', show_defau
     cred = get_creds(file=CRED_FILE)
     tab_creds = cred['Tableau']
     conf = get_conf(file=CONF_FILE)
+
+    zs = Zabbix_sender(item_key=SCRIPT_NAME)
+
+    if zab_test:
+        zs.send(1)
+        sys.exit(0)
 
     tpc = TableauPermissionCleaner(server=tab_creds['server'],
                                    username=tab_creds['username'],
